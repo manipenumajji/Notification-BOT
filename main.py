465107bbdd2de95995bf852f23000efa24079eec
manipenumajji <manipenumajji0@gmail.com>
2026-06-18 11:57:34 +0530

    Completed Telegram alert integration

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 import time
+
 from price_fetcher import get_price
 from alert import check_alert
-from notification import play_alert , show_notification
-target = float(input("enter target price:"))
-alert_sent=False
+from notification import play_alert, show_notification
+from telegram_notification import send_telegram_message
+
+target = float(input("Enter target price: "))
+
+alert_sent = False
+
 while True:
-    price=get_price()
-    print("current price:",price) 
-    if check_alert(price,target) and not alert_sent:
-        show_notification(price, target)
+
+    price = get_price()
+
+    print(f"Current Price: {price}")
+
+    if check_alert(price, target) and not alert_sent:
+
         play_alert()
-        print("Alert Triggred")
-        alert_sent=True
-    time.sleep(10)    
 
+        show_notification(price, target)
 
+        message = f"""
+🚨 BTC ALERT 🚨
 
+Current Price: {price}
+Target Price: {target}
+"""
+
+        send_telegram_message(message)
+
+        print("Alert Triggered")
+
+        alert_sent = True
+
+    time.sleep(10)
